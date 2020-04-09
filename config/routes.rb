7eca28ba1efc107dcc149932810224f905110144
Howard Kuang <kku504@gmail.com>
2020-04-08 22:04:16 -0500

    still nor working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  #resources :articles 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'pages#home'
-  get 'about', to: 'pages#aboutus'
-  get 'article', to: 'articles#index'
+  resources :articles 
 end
